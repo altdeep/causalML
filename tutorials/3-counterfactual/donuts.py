4711ac12947af6b8822b7673be6b295dc97a0ddc
Sam Stites <stites@users.noreply.github.com>
2020-12-20 23:22:05 -0500

    add Lily's work

--- tutorials/3-counterfactual/donuts.py
+++ tutorials/3-counterfactual/donuts.py
@@ -1,14 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Concentric Circles - "Donuts" Dataset
+
+# In[24]:
+
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
-
 from torch.utils.data import Dataset, DataLoader
-
 from pyro import poutine
-
 import torch
+import pyro
+
+
+# # Donuts Dataset Class
+
+# #### Data Generator for the Donuts Tutorial
+
+# From sklearn's [make_circles](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html) function, we create a dataset with chosen noise and factor variables:
+# * noise - Standard deviation of Gaussian noise added to the data as a double value
+# * factor - Scale factor between inner and outer circle as a double value ranging from 0 to 1
+
+# DonutsDataset's make_dataset returns a dataset containing:
+# * x1 - array of the generated samples of x1 from the pairs of X
+# * x2 - array of the generated samples of x2 from the pairs of X
+# * label - array of the the integer labels (0 or 1) for class membership of each X sample
+# * noise - the noise value defined in data creation
+# * factor - the factor value defined in data creation
+
+# This is the data generating process that is modeled in the counterfactual_donuts_tutorial and that counterfactual inference is performed for.
+
+# In[25]:
 
 
 class DonutsDataset(Dataset):
@@ -41,6 +67,11 @@ class DonutsDataset(Dataset):
         return len(self.data.values())
 
 
+# # Plot Function for Visualization of the Dataset
+
+# In[26]:
+
+
 def plot(x1, x2, y):
     X = StandardScaler().fit_transform(np.concatenate([x1, x2], axis=-1))
 
@@ -48,6 +79,9 @@ def plot(x1, x2, y):
     plt.xlabel(r'$x_1$')
     plt.ylabel(r'$x_2$')
     plt.scatter(X[:,0], X[:,1], alpha=0.5, color=['red' if y_ == 1 else 'green' for y_ in y])
+    ax = plt.gca()
+    ax.set_xlim(-3,3)
+    ax.set_ylim(-3,3)
     plt.show()
 
     plt.subplot(1, 2, 1)
@@ -55,6 +89,7 @@ def plot(x1, x2, y):
                  bins=None,
                  hist_kws={'edgecolor':'black'},
                  kde_kws={'linewidth': 2})
+
     plt.title(r'$p(x_1)$')
     plt.subplot(1, 2, 2)
     sns.distplot(X[:,1], hist=False, kde=True,
@@ -63,6 +98,12 @@ def plot(x1, x2, y):
                  kde_kws={'linewidth': 2})
     plt.title(r'$p(x_2)$')
     plt.show()
+
+
+# # Plot Function for Visualization of the Predicted (x1,x2) pairs and the Original Dataset
+
+# In[27]:
+
 
 def compare(model, dataset, n=1000):
     if isinstance(model, tuple):
@@ -75,44 +116,99 @@ def compare(model, dataset, n=1000):
     plt.title(r'Joint Distribution')
     plt.xlabel(r'$x_1$')
     plt.ylabel(r'$x_2$')
-    plt.scatter(X1, X2, label='data', alpha=0.5, color=['red' if y_ == 1 else 'green' for y_ in Y])
-    plt.scatter(x1, x2, label='flow', alpha=0.5, color=['firebrick' if y_ == 1 else 'blue' for y_ in y])
+    plt.scatter(X1, X2, label='data', alpha=0.5, color=['red' if y_ == 1 else 'red' for y_ in Y])
+    ax = plt.gca()
+    ax.set_xlim(-3,3)
+    ax.set_ylim(-3,3)
+    plt.scatter(x1, x2, label='flow', alpha=0.5, color=['blue' if y_ == 1 else 'blue' for y_ in y])
+    ax = plt.gca()
+    ax.set_xlim(-3,3)
+    ax.set_ylim(-3,3)
     plt.legend()
     plt.show()
 
     plt.subplot(1, 2, 1)
     sns.distplot(X1, hist=False, kde=True, 
-                 bins=None,
+                 bins=None,color='red',
                  hist_kws={'edgecolor':'black'},
                  kde_kws={'linewidth': 2},
                  label='data')
     sns.distplot(x1, hist=False, kde=True, 
-                 bins=None, color='firebrick',
+                 bins=None, color='blue',
                  hist_kws={'edgecolor':'black'},
                  kde_kws={'linewidth': 2},
                  label='flow')
     plt.title(r'$p(x_1)$')
     plt.subplot(1, 2, 2)
-    sns.distplot(X1, hist=False, kde=True, 
-                 bins=None,
+    sns.distplot(X2, hist=False, kde=True, 
+                 bins=None,color='red',
                  hist_kws={'edgecolor':'black'},
                  kde_kws={'linewidth': 2},
                  label='data')
-    sns.distplot(x1, hist=False, kde=True, 
-                 bins=None, color='firebrick',
+    sns.distplot(x2, hist=False, kde=True, 
+                 bins=None, color='blue',
                  hist_kws={'edgecolor':'black'},
                  kde_kws={'linewidth': 2},
                  label='flow')
     plt.title(r'$p(x_2)$')
     plt.show()
 
-def factor_mechanism(noise, N_factor):
-    return sigmoid(N_factor + noise)
 
-def sample_trained(plate_model, n=1000):
-    handler = poutine.trace(plate_model)
-    trace = handler.get_trace(n)
-    nodes = ['x1', 'x2', 'label', 'factor', 'noise']
-    x1, x2, y, factor, noise = [trace.nodes[node]['value'].detach().numpy() for node in nodes]
-    return x1, x2, y, factor, noise
+# # Flow-Based PyroModule Framework Helper Functions
+
+# In[28]:
+
+
+from functools import reduce
+import sys
+from functools import partial
+
+def sumall(tens):
+    reified = list(tens)  # NOTE: Need the lookahead for initializer, this is an iterator
+    initializer = torch.zeros_like(reified[0])
+    reducer = lambda l, memo: memo + l
+    return reduce(reducer, reified, initializer)
+
+def get_logprobs(tr, k):
+    site = tr.nodes[k]
+    assert site["type"] == "sample" and site["is_observed"], f'{k} is type {site["type"]} and is_observed: {site["is_observed"]}'
+    return site["log_prob"]
+
+def visualize(dataset, model):
+    with pyro.plate('samples', 1000):
+        outs = model.model()
+        x1_flow = outs[0]
+        x2_flow = outs[1]
+        y_flow = torch.ones_like(x1_flow)
+    compare((x1_flow.detach(), x2_flow.detach(), y_flow.detach()), dataset)
+    
+def smoke_test(model):
+    print(model.model())
+    with pyro.plate('test', 13):
+        out = model.model()
+        print(out[0].shape)
+    sleep(0.5)
+
+
+# ## Helper function for Conditional Affine Transforms made by [N. Pawlowski+, D. C. Castro+, B. Glocker.](https://github.com/biomedia-mira/deepscm)
+
+# In[31]:
+
+
+from pyro.distributions.conditional import ConditionalTransformModule, ConditionalTransformedDistribution
+
+# from deepscm.distributions.transforms.affine.py
+class ConditionalAffineTransform(ConditionalTransformModule):
+    def __init__(self, context_nn, event_dim=0, **kwargs):
+        super().__init__(**kwargs)
+
+        self.event_dim = event_dim
+        self.context_nn = context_nn
+
+    def condition(self, context):
+        loc, log_scale = self.context_nn(context)
+        scale = torch.exp(log_scale)
+
+        ac = T.AffineTransform(loc, scale, event_dim=self.event_dim)
+        return ac
 
