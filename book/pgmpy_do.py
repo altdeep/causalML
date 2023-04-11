d68ec8c1b2d83497cbddfb14837338612218c927
Robert Osazuwa Ness <robertness@gmail.com>
2023-04-11 09:41:57 -0600

    Create pgmpy_do.py
    A function for ideal (atomic) interventions in pgmpy

---
+++ book/pgmpy_do.py
@@ -0,0 +1,36 @@
+def do(model, interventions):
+    """ 
+    Implement an ideal intervention for discrete variables. Modifies pgmpy's
+    `do` method so it is a `do`-operator, meaning a function that takes in a
+    model, modifies it with an ideal intervention, and returns a new model.
+    Note that this code would need to be modified to work for continuous
+    variables.
+    """
+    def _mod_kernel(kernel, int_val):
+        """
+        Modify a causal Markov kernel so all probability is on the state fixed
+        by the intervention.
+        """ 
+        var_name = kernel.variable
+        card = kernel.get_cardinality([var_name])[var_name]
+        states = [kernel.get_state_names(var_name, i) for i in range(card)]
+        non_int_states = set(states) - {int_val,}
+        unordered_prob_vals = [[1.0]] + [[0.0] for _ in range(card - 1)]
+        unordered_states = [int_val] + list(non_int_states)
+        # Reorder so it matches original
+        dict_ = dict(zip(unordered_states, unordered_prob_vals))
+        ordered_prob_values = [dict_[k] for k in states]
+        intervention_kernel = TabularCPD(
+            var_name, card, ordered_prob_values,
+            state_names = {var_name: states}
+        )
+        return intervention_kernel
+
+    kernels = {kern.variable: kern for kern in model.get_cpds()}
+    new_model = model.copy()
+    for var, int_val in interventions.items():
+        new_model = new_model.do(var)
+        new_kernel = _mod_kernel(kernels[var], int_val)
+        new_model.add_cpds(new_kernel)
+    return new_model
+    
