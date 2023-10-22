52fc99732cc5a702c52eb179ea4198d14cc83578
Robert Osazuwa Ness <robertness@gmail.com>
2023-10-22 23:07:00 +0100

    Added clone function

--- book/chapter 9/hyp_function.py
+++ book/chapter 9/hyp_function.py
@@ -1,69 +1,37 @@
+from copy import deepcopy
 from pgmpy.factors.discrete.CPD import TabularCPD
 
 
-#A Clone of the assignment function for the host door selection for use in the hypothetical world.
-f_host_door_selection_hyp = TabularCPD(
-    variable='Host Door Selection Hyp',
-    variable_card=3,
-    values=[
-        [0,0,0,0,1,1,0,1,1,0,0,0,0,0,1,0,1,0],
-        [1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,1],
-        [0,1,0,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0]
-    ],
-    evidence=['Coin Flip', 'Car Door Die Roll', '1st Choice Die Roll'],
-    evidence_card=[2, 3, 3],
-    state_names={
-        'Host Door Selection Hyp':['1st', '2nd', '3rd'],
-        'Coin Flip': ['tails', 'heads'],
-        'Car Door Die Roll': ['1st', '2nd', '3rd'],
-        '1st Choice Die Roll': ['1st', '2nd', '3rd']
-    }
-)
+ENDOGENOUS_VARS = ["Host Door Selection", "Strategy", "2nd Choice", "Win or Lose"]
 
-#B Clone of the assignment function for strategy for use in the hypothetical world.
-f_strategy_hyp = TabularCPD(
-    variable='Strategy Hyp',
-    variable_card=2,
-    values=[[1, 0], [0, 1]],
-    evidence=['Coin Flip'],
-    evidence_card=[2],
-    state_names={
-        'Strategy Hyp': ['stay', 'switch'],
-        'Coin Flip': ['tails', 'heads']}
-)
-
-#C Clone of the assignment function for the player's second choice for use in the hypothetical world.
-f_second_choice_hyp = TabularCPD(
-    variable='2nd Choice Hyp',
-    variable_card=3,
-    values=[
-        [1,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,1,0],
-        [0,1,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,1],
-        [0,0,1,0,0,1,0,0,1,0,1,0,1,0,0,0,0,0]
-    ],
-    evidence=['Strategy Hyp', 'Host Door Selection Hyp', '1st Choice Die Roll'],
-    evidence_card=[2, 3, 3],
-    state_names={
-        '2nd Choice Hyp': ['1st', '2nd', '3rd'],
-        'Strategy Hyp': ['stay', 'switch'],
-        'Host Door Selection Hyp': ['1st', '2nd', '3rd'],
-        '1st Choice Die Roll': ['1st', '2nd', '3rd']
-    }
-)
-
-#D Clone of the assignment function for the win/loss outcome of the game for use in the hypothetical world.
-f_win_or_lose_hyp = TabularCPD(
-    variable='Win or Lose Hyp',
-    variable_card=2,
-    values=[
-        [1,0,0,0,1,0,0,0,1],
-        [0,1,1,1,0,1,1,1,0],
-    ],
-    evidence=['2nd Choice Hyp', 'Car Door Die Roll'],
-    evidence_card=[3, 3],
-    state_names={
-        'Win or Lose Hyp': ['win', 'lose'],
-        '2nd Choice Hyp': ['1st', '2nd', '3rd'],
-        'Car Door Die Roll': ['1st', '2nd', '3rd']
-    }
-)
+def clone(cpd: TabularCPD):
+    endogenous_vars = ENDOGENOUS_VARS
+    cpd_hyp = deepcopy(cpd)
+    cpd_hyp.variable += " Hyp"
+    variables = cpd_hyp.variables
+    new_variables = []
+    new_state_names = {}
+    new_name_to_no = {}
+    new_no_to_name = {}
+    for var_name, val in cpd_hyp.state_names.items():
+        if var_name in endogenous_vars:
+            new_variables.append(var_name + " Hyp")
+            new_state_names[var_name + " Hyp"] = val
+        else:
+            new_variables.append(var_name)
+            new_state_names[var_name] = val
+    for var_name, val in cpd_hyp.name_to_no.items():
+        if var_name in endogenous_vars:
+            new_name_to_no[var_name + " Hyp"] = val
+        else:
+            new_name_to_no[var_name] = val
+    for var_name, val in cpd_hyp.no_to_name.items():
+        if var_name in endogenous_vars:
+            new_no_to_name[var_name + " Hyp"] = val
+        else:
+            new_no_to_name[var_name] = val
+    cpd_hyp.variables = new_variables
+    cpd_hyp.state_names = new_state_names
+    cpd_hyp.name_to_no = new_name_to_no
+    cpd_hyp.no_to_name = new_no_to_name
+    return cpd_hyp
