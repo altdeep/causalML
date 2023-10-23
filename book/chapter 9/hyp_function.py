604230de3d1c92b2a8745286123078811c1b6c7a
Robert Osazuwa Ness <robertness@gmail.com>
2023-10-23 15:00:28 +0100

    Update hyp_function.py

--- book/chapter 9/hyp_function.py
+++ book/chapter 9/hyp_function.py
@@ -4,10 +4,11 @@ from pgmpy.factors.discrete.CPD import TabularCPD
 
 ENDOGENOUS_VARS = ["Host Door Selection", "Strategy", "2nd Choice", "Win or Lose"]
 
-def clone(cpd: TabularCPD):
+def clone(cpd: TabularCPD, suffix: str="Hyp"):
+    suffix = " " + suffix
     endogenous_vars = ENDOGENOUS_VARS
     cpd_hyp = deepcopy(cpd)
-    cpd_hyp.variable += " Hyp"
+    cpd_hyp.variable += suffix
     variables = cpd_hyp.variables
     new_variables = []
     new_state_names = {}
@@ -15,19 +16,19 @@ def clone(cpd: TabularCPD):
     new_no_to_name = {}
     for var_name, val in cpd_hyp.state_names.items():
         if var_name in endogenous_vars:
-            new_variables.append(var_name + " Hyp")
+            new_variables.append(var_name + suffix)
             new_state_names[var_name + " Hyp"] = val
         else:
             new_variables.append(var_name)
             new_state_names[var_name] = val
     for var_name, val in cpd_hyp.name_to_no.items():
         if var_name in endogenous_vars:
-            new_name_to_no[var_name + " Hyp"] = val
+            new_name_to_no[var_name + suffix] = val
         else:
             new_name_to_no[var_name] = val
     for var_name, val in cpd_hyp.no_to_name.items():
         if var_name in endogenous_vars:
-            new_no_to_name[var_name + " Hyp"] = val
+            new_no_to_name[var_name + suffix] = val
         else:
             new_no_to_name[var_name] = val
     cpd_hyp.variables = new_variables
