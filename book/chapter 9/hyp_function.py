d6ef10f3a4e719d801e9d7135c803f04e8969896
Robert Osazuwa Ness <robertness@gmail.com>
2023-10-23 15:08:57 +0100

    Commenting hyp_function.py

--- book/chapter 9/hyp_function.py
+++ book/chapter 9/hyp_function.py
@@ -5,6 +5,16 @@ from pgmpy.factors.discrete.CPD import TabularCPD
 ENDOGENOUS_VARS = ["Host Door Selection", "Strategy", "2nd Choice", "Win or Lose"]
 
 def clone(cpd: TabularCPD, suffix: str="Hyp"):
+    """
+    This is a helper function for use in copying TabularCPD objects. It is used
+    to implement a parallel world graphical model for counterfactual reasoning
+    in Chapter 9 of the book Causal AI. https://www.manning.com/books/causal-ai
+    It assumes there are a set of endogenous variables that are meant to be
+    copied.  For those variables, a suffix is added to the name to distinquish
+    those variables from their original. The code works by apply deepcopy, then
+    changing the "variable", "variables", "state_names", "name_to_no", and
+    "no_to_name" attributes of the TabularCPD class.
+    """
     suffix = " " + suffix
     endogenous_vars = ENDOGENOUS_VARS
     cpd_hyp = deepcopy(cpd)
@@ -17,7 +27,7 @@ def clone(cpd: TabularCPD, suffix: str="Hyp"):
     for var_name, val in cpd_hyp.state_names.items():
         if var_name in endogenous_vars:
             new_variables.append(var_name + suffix)
-            new_state_names[var_name + " Hyp"] = val
+            new_state_names[var_name + suffix] = val
         else:
             new_variables.append(var_name)
             new_state_names[var_name] = val
